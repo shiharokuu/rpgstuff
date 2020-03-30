c1ed3b0200b4e29cb70f4a3022bd4e86e9ce0a91
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2020-03-30 01:10:14 -0600

    Add files via upload

---
+++ shirou.js
@@ -0,0 +1,4 @@
+var videos = ['qnlzyCCgFBM', 't3dIokufs0M','CWoynz0daB8'];    
+var ytCode = videos[Math.floor(Math.random() * videos.length)];
+var url="https://www.youtube.com/embed/" + ytCode + "?controls=0&modestbranding=1&rel=0";
+document.getElementById('shero').src = url;
