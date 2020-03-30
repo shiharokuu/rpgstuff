7e78494c164870dcd2f94a82da0bd06e46b0424e
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2020-03-30 02:16:28 -0600

    Update shirou.js

--- shirou.js
+++ shirou.js
@@ -1,4 +1,17 @@
+if(trace==null){
+	var trace;
+}
+
 var videos = ['qnlzyCCgFBM', 't3dIokufs0M','CWoynz0daB8'];    
 var ytCode = videos[Math.floor(Math.random() * videos.length)];
 var url="https://www.youtube.com/embed/" + ytCode + "?controls=0&modestbranding=1&rel=0";
-document.getElementById('shero').src = url;
+
+if (trace==null) {
+ document.getElementById('shero1').src = url;
+  trace="on";
+} else if (trace="on") {
+  document.getElementById('shero2').src = url;
+  trace="off";
+} else {
+  document.getElementById('shero3').src = url;
+}
