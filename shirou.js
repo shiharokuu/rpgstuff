bdd23364bdf2d9b60e82922d2ca1b115be4c6df0
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2020-03-30 01:43:43 -0600

    Update shirou.js

--- shirou.js
+++ shirou.js
@@ -1,6 +1,4 @@
-function myVideo() {
 var videos = ['qnlzyCCgFBM', 't3dIokufs0M','CWoynz0daB8'];    
 var ytCode = videos[Math.floor(Math.random() * videos.length)];
 var url="https://www.youtube.com/embed/" + ytCode + "?controls=0&modestbranding=1&rel=0";
 document.getElementById('shero').src = url;
-}
