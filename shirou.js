1c9949140f5972202d476202074ecbe029c652f2
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2020-03-30 03:08:23 -0600

    Update shirou.js

--- shirou.js
+++ shirou.js
@@ -5,11 +5,12 @@ $('.shero').each(function(){
     $(this).attr('id',newID);
     $(this).val(i);
 });
-if(s==null){
-	var s=1;
+
+var j;
+for (j = 1; j <=i; j++) {
+	var videos = ['qnlzyCCgFBM', 't3dIokufs0M','CWoynz0daB8'];    
+	var ytCode = videos[Math.floor(Math.random() * videos.length)];
+	var url="https://www.youtube.com/embed/" + ytCode + "?controls=0&modestbranding=1&rel=0";
+	document.getElementById("shero" + j).src = url;
+
 }
-var videos = ['qnlzyCCgFBM', 't3dIokufs0M','CWoynz0daB8'];    
-var ytCode = videos[Math.floor(Math.random() * videos.length)];
-var url="https://www.youtube.com/embed/" + ytCode + "?controls=0&modestbranding=1&rel=0";
-document.getElementById("shero" + s).src = url;
-s++;
