24672b1ba5f95ae988336b572e3c05b631ea438c
shiharokuu <62873007+shiharokuu@users.noreply.github.com>
2020-04-12 17:06:29 -0600

    Create emiyaSTATS.js

---
+++ emiyaSTATS.js
@@ -0,0 +1,16 @@
+function getHP(vit, res) {
+  var base = 50;
+  return base + (vit*10)+(Math.round(res/2));   // The function returns the HP of my character
+}
+
+function getMP(prana, mana) {
+  var base = 40;
+  return base + (prana*10)+(Math.round(mana/2));   // The function returns the MP of my character
+}
+var vit = document.getElementById("hp").getAttribute("vit");
+var res = document.getElementById("hp").getAttribute("res");
+var prana = document.getElementById("mp").getAttribute("prana");
+var mana = document.getElementById("mp").getAttribute("mana");
+
+document.getElementById("hp").innerHTML = getHP(vit,res);
+document.getElementById("mp").innerHTML = getMP(prana,mana);
